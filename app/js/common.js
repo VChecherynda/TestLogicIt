4ff1acb7ec9808ab06c104cbfc952c651aa2af58
Чечеринда Вадим <checherinda.vadym@gmail.com>
2017-07-29 01:33:44 +0300

    Make responsive menu and transition

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	$('.nav-icon').click(function(e){
+	$('.icon-mobile').click(function(e){
 		e.preventDefault();
 		$(this).stop().toggleClass('open');
 		$('.nav-mobile').stop().toggleClass('open');
