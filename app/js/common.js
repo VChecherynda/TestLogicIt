4c64f16ed4a4b7b850ae6a83a925afdb66abaccc
Чечеринда Вадим <checherinda.vadym@gmail.com>
2017-07-29 20:16:48 +0300

    Refactored

--- app/js/common.js
+++ app/js/common.js
@@ -2,8 +2,9 @@ $(function() {
 
 	$('.icon-mobile').click(function(e){
 		e.preventDefault();
+		$(this).parent().toggleClass('nav-mobile');
 		$(this).stop().toggleClass('open');
-		$('.nav-mobile').stop().toggleClass('open');
+		$('.menu').stop().toggleClass('mobile');
 	});
 
 });
